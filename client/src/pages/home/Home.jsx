c6b9625ba7c01ddb107fe2bd9cb861daca480811
devplus-huuchinh <99627739+devplus-huuchinh@users.noreply.github.com>
2022-06-10 20:45:42 +0700

    fix: 🐛 404 error code

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,30 +1,28 @@
-import { useEffect, useState } from "react"
-import Header from "../../components/header/Header"
-import Posts from "../../components/posts/Posts"
-import Sidebar from "../../components/sidebar/Sidebar"
-import "./home.css"
-import axios from "axios"
-
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
 
-  const [posts,setPosts] = useState([]);
-
-  useEffect(()=>{
-    const fetchPosts = async ()=>{
-      const res = await axios.get("/posts")
-      setPosts(res.data)
-    }
-    fetchPosts()
-  },[])
-
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("http://localhost:5000/api/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+  console.log(posts);
   return (
     <>
-      <Header/>
-      <div className="home" >
-        <Posts posts={posts} ></Posts>
-        <Sidebar/>
+      <Header />
+      <div className="home">
+        <Posts posts={posts}></Posts>
+        <Sidebar />
       </div>
     </>
-  )
+  );
 }
